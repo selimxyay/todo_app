202e130e85e6d8e73686ee0f2620dd38fed20577
Glopark <glopark@192.168.1.81>
2024-11-15 14:17:12 +0300

    separated the logic | optimized the codebase

---
+++ lib/logic/home_page_logic.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:todo_app/data/todo_database.dart';
+import 'package:todo_app/pages/home_page.dart';
+import 'package:todo_app/widgets/create_new_task_dialog.dart';
+
+mixin HomePageLogic on State<HomePage> {
+  final todo = Hive.box('todo');
+  final db = TodoDatabase();
+  final task = TextEditingController();
+
+  void onCheckBoxValueChanged({
+    required bool? value,
+    required int index,
+  }) {
+    setState(() {
+      db.todoList[index][1] = !db.todoList[index][1];
+      db.updateData();
+    });
+  }
+
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return CreateNewTaskDialog(
+          controller: task,
+          onCancel: () => Navigator.of(context).pop(),
+          onSave: saveNewTask,
+        );
+      },
+    );
+  }
+
+  void saveNewTask() {
+    setState(() {
+      db.todoList.add([
+        task.text,
+        false,
+      ]);
+    });
+    task.clear();
+    Navigator.of(context).pop();
+    db.updateData();
+  }
+
+  void deleteTask({required int index}) {
+    setState(() {
+      db.todoList.removeAt(index);
+    });
+    db.updateData();
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:todo_app/data/todo_database.dart';
-import 'package:todo_app/widgets/create_new_task_dialog.dart';
-
-import '../widgets/todo_container.dart';
+import 'package:todo_app/logic/home_page_logic.dart';
+import 'package:todo_app/widgets/todo_container.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,83 +9,30 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-
+class _HomePageState extends State<HomePage> with HomePageLogic {
   @override
   void initState() {
     super.initState();
-    /// if the user is opening the app for the first time ever
-    /// execute the initialData function
-    /// else, just load the data by executing the loadData function
-    if(todo.get('todoList') == null) {
-      db.initialData();
-    } else {
-      db.loadData();
-    }
+    todo.get('todoList') == null 
+    ? db.initialData()
+    : db.loadData();
   }
-
-  final todo = Hive.box('todo');
-  final db = TodoDatabase();
-  final createNewTaskInputController = TextEditingController();
-
-  /// method that updates the value of checkbox which allow users to toggle. 
-  /// in the context of programming, toggle basically means switching between two states, like true or false.
-  void onCheckBoxValueChanged({required bool? value, required int index}) {
-    setState(() {
-      db.todoList[index][1] = !db.todoList[index][1];
-      db.updateData();
-    });
-  }
- 
-  /// when users clicked on floating action button for creating a new task
-  /// this method will be executed.
-  void createNewTask() {
-    showDialog(
-      context: context, 
-      builder: (context) {
-        return CreateNewTaskDialog(
-          controller: createNewTaskInputController,
-          onCancel: () => Navigator.of(context).pop(),
-          onSave: saveNewTask,
-        );
-      },
-    );
-  }
-
-  /// when users clicked on save, this method will be executed and a new task will be added to the list. 
-  void saveNewTask() {
-    setState(() {
-      db.todoList.add([
-        createNewTaskInputController.text,
-        false,
-      ]);
-    }); 
-    createNewTaskInputController.clear();
-    Navigator.of(context).pop();
-    db.updateData();
-  }
-
-  // delete task
-  void deleteTask({required int index}) {
-    setState(() {
-      db.todoList.removeAt(index);
-    });
-      db.updateData();
-  }
-
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).colorScheme;
     return Scaffold(
       backgroundColor: Colors.yellow[200],
-      appBar: AppBar(backgroundColor: theme.primaryContainer, title: const Text('TO DO APP')),
+      appBar: AppBar(
+        backgroundColor: theme.primaryContainer,
+        title: const Text('TO DO APP'),
+      ),
       body: ListView.builder(
         itemCount: db.todoList.length,
         itemBuilder: (context, builderIndex) {
           return ToDoContainer(
-            taskName: db.todoList[builderIndex][0], 
-            isTaskCompleted: db.todoList[builderIndex][1], 
+            taskName: db.todoList[builderIndex][0],
+            isTaskCompleted: db.todoList[builderIndex][1],
             onChanged: (newValue) => onCheckBoxValueChanged(index: builderIndex, value: newValue),
             onDelete: () => deleteTask(index: builderIndex),
           );
